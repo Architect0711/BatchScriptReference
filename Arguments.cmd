7ca90b59ef899b9caaf46cfebb69c1edaf581d7c
Robert <r.bantele@live.de>
2020-08-25 00:50:34 +0200

    Improved Readability

--- Arguments.cmd
+++ Arguments.cmd
@@ -1,5 +1,8 @@
 ECHO OFF
 
+REM -----------------------------------------------------------------------------------
+REM Save the Arguments to Variables
+REM -----------------------------------------------------------------------------------
 REM Variable to hold Argument Nr. 1
 SET ARG_1=%1
 REM Variable to hold Argument Nr. 2
@@ -7,6 +10,9 @@ SET ARG_2=%2
 REM Variable to hold Argument Nr. 3
 SET ARG_3=%3
 
+REM -----------------------------------------------------------------------------------
+REM Output the Arguments to the Console
+REM -----------------------------------------------------------------------------------
 REM Output Argument Nr. 1 to the Console
 ECHO %ARG_1%
 REM Output Argument Nr. 2 to the Console
@@ -14,5 +20,7 @@ ECHO %ARG_2%
 REM Output Argument Nr. 3 to the Console
 ECHO %ARG_3%
 
+REM ----------------------------------------------------------------------------------
 REM Wait after the Script is finished so the User can read the Output
+REM -----------------------------------------------------------------------------------
 TIMEOUT 5
--- PythonRelativePath.cmd
+++ PythonRelativePath.cmd
@@ -1,5 +1,8 @@
 ECHO OFF
 
+REM -----------------------------------------------------------------------------------
+REM Construct the Paths
+REM -----------------------------------------------------------------------------------
 REM Get the Location of this Batch Script
 SET LOCAL_PATH=%~dp0
 REM Enter the Name of the Python Script that should be started
@@ -9,10 +12,15 @@ SET PYTHON_SCRIPT_PATH=%LOCAL_PATH%%PYTHON_SCRIPT_NAME%
 REM Build the full Path to the Python Executable here
 SET PYTHON_EXE_PATH=%LOCAL_PATH%venv\Scripts\Python.exe
 
+REM -----------------------------------------------------------------------------------
+REM Call the Python Script
+REM -----------------------------------------------------------------------------------
 REM Start the Script with the specified Python Executable
 REM Use the -i Flag to launch Python in interactive Mode
 REM https://docs.python.org/3/using/cmdline.html#cmdoption-i
 %PYTHON_EXE_PATH% -i %PYTHON_SCRIPT_PATH%
 
+REM -----------------------------------------------------------------------------------
 REM Wait after the Script is finished so the User can read the Output
+REM -----------------------------------------------------------------------------------
 TIMEOUT 5

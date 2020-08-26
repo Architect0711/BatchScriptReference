d7b8825aaa1cc887d3473d75d88f623396f07df5
Robert <r.bantele@live.de>
2020-08-26 20:10:25 +0200

    Added FOR_LOOP.cmd

---
+++ FOR_LOOP.cmd
@@ -0,0 +1,26 @@
+ECHO OFF
+
+REM -----------------------------------------------------------------------------------
+REM Construct the Paths
+REM -----------------------------------------------------------------------------------
+REM Get the Location of this Batch Script
+SET LOCAL_PATH=%~dp0
+
+REM -----------------------------------------------------------------------------------
+REM FOR LOOP - SINGLE LINE
+REM -----------------------------------------------------------------------------------
+REM Print all File Names in the current Directory
+FOR %%i IN (%LOCAL_PATH%\*) DO ECHO %%i 
+
+REM -----------------------------------------------------------------------------------
+REM FOR LOOP - MULTILINE
+REM -----------------------------------------------------------------------------------
+REM Print all Numbers from a List of Numbers from 1 to 100
+FOR /l %%i IN (1, 1, 100) DO (
+	ECHO %%i 
+)
+
+REM -----------------------------------------------------------------------------------
+REM Wait after the Script is finished so the User can read the Output
+REM -----------------------------------------------------------------------------------
+TIMEOUT 5
--- README.md
+++ README.md
@@ -6,6 +6,10 @@ A Home for Snippets of all the Batch Scripts I made over Time.
 
 Pass Arguments to the Batch Script and save them in Variables.
 
+## FOR_LOOP.cmd
+
+Single and Multi Line FOR LOOP Snippets and a List Declaration.
+
 ## IF_ELSE.cmd
 
 Single and Multi Line IF ELSE Snippets and GOTO calls.

0910589dcaa797c9bdbf00683e4dbc674e846c5a
Robert <r.bantele@live.de>
2024-12-05 11:54:50 +0100

    added start program and exit

---
+++ StartProgramAndExit.cmd
@@ -0,0 +1,7 @@
+REM ECHO OFF
+
+REM -----------------------------------------------------------------------------------
+REM Start a program and exit the console window
+REM See https://stackoverflow.com/questions/5909012/windows-batch-script-launch-program-and-exit-console
+REM -----------------------------------------------------------------------------------
+start "" "%SystemRoot%\Notepad.exe"
